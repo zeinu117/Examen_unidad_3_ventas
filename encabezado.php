e27d5321cafcf97eb30d962248d1e57071c63682
zeinu117 <cesar_zein@hotmail.com>
2021-06-28 16:17:20 -0500

    version 1

---
+++ encabezado.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Ventas</title>
+	
+	<link rel="stylesheet" href="./css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="./css/2.css">
+	<link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body class="">
+	<nav class="navbar  navbar-fixed-top" style="background-color: #FA6109;">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#"></a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a style="color: black;" href="./listar.php">Productos</a></li>
+					<li><a style="color: black;" href="./vender.php">Vender</a></li>
+					<li><a style="color: black;" href="./ventas.php">Ventas</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row">
+		
